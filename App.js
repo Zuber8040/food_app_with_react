f842855350b34320663b13b9b82676ab24570608
Zuber Ustad <72431298+Zuber8040@users.noreply.github.com>
2024-11-26 08:52:15 +0530

    react router added to the project

--- App.js
+++ App.js
@@ -2,15 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./src/components/Body";
 import Headers from "./src/components/Header";
-
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import About from "./src/components/About";
+import Error from './src/components/Error'
+import Contact from './src/components/Contact'
 const AppLayout = () => {
     return (
         <div className="app">
             <Headers />
-            <Body />
+            <Outlet />
         </div>
     )
-}
+};
+
+const appRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Body />,
+            },
+            {
+                path: '/about',
+                element: <About />
+            },
+            {
+                path: '/contact',
+                element: <Contact />
+            },
+
+        ],
+        errorElement: <Error />
+    },
+])
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is react webseries</h1>
+            <h2>This is about react</h2>
+        </div>
+    )
+}
+
+export default About
